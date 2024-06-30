95c8765d27fc0aa1029a44c1e122a8bfb88a3f2f
Siriulas <108139838+Siriulas@users.noreply.github.com>
2024-06-30 22:17:40 +0300

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -13,6 +13,8 @@ const secondsSpan = document.querySelector('[data-seconds]');
 let timerInterval;
 let userSelectedDate;
 
+startButton.disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -70,19 +72,14 @@ function addLeadingZero(value) {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
